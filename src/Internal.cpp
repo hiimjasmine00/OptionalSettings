278360239cd05d59dca8412608db02ccc9791a2a
Jasmine <52604018+hiimjasmine00@users.noreply.github.com>
2025-11-01 12:03:30 -0400

    One less copy

--- src/Internal.cpp
+++ src/Internal.cpp
@@ -1,10 +1,10 @@
 #include "Internal.hpp"
-#include <Geode/loader/Mod.hpp>
+#include <Geode/loader/ModSettingsManager.hpp>
 
 using namespace geode::prelude;
 
 void Internal::registerCustomSettingType(std::string_view type, SettingGeneratorV3 generator) {
-    if (auto res = Mod::get()->registerCustomSettingType(type, std::move(generator)); res.isErr()) {
+    if (auto res = ModSettingsManager::from(getMod())->registerCustomSettingType(type, std::move(generator)); res.isErr()) {
         log::error("Failed to register custom setting type '{}': {}", type, std::move(res).unwrapErr());
     }
 }
